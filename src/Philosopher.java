a1d2a0fca65b685cc1dd12ec0843be9165add7f3
Justin Cotner <jdc9622@rit.edu>
2014-09-26 13:30:06 -0400

    Commiting skeleton classes

---
+++ src/Philosopher.java
@@ -0,0 +1,13 @@
+
+public class Philosopher extends Thread{
+	
+	public Philosopher(int id, Fork left, Fork right, boolean rHanded,
+			int nTimes, long thinkMillis, long eatMillies){
+		
+	}
+	
+	public void run(){
+		
+	}
+
+}
