31c149ec06bfc0147de135f6119de63ee7fe3410
Justin Cotner <jdc9622@rit.edu>
2014-09-29 10:09:36 -0400

    Finished philosopher class

--- src/Philosopher.java
+++ src/Philosopher.java
@@ -44,13 +44,40 @@ public class Philosopher extends Thread{
 				System.out.println("Philospher " + id + " has right fork.");
 				
 				yield();
-				
-				System.out.println("Philosopher " + id + " goes for left fork.");
-				
-				left.acquire();
-				
-				System.out.println("Philosopher " + id + " has left fork.");
 			}
+			
+			System.out.println("Philosopher " + id + " goes for left fork.");
+			
+			left.acquire();
+			
+			System.out.println("Philosopher " + id + " has left fork.");
+			
+			yield();
+			
+			if(!rHanded){
+				System.out.println("Philosopher " + id + " goes for right fork.");
+				
+				right.acquire();
+				
+				System.out.println("Philospher " + id + " has right fork.");
+			}
+			
+			t = rand.nextInt(((int)eatMillis/1000));
+			
+			System.out.println("Philosopher " + id + " eats for " + t + " time units.");
+			
+			try{
+				sleep(t * 1000);
+			}
+			catch(InterruptedException e){}
+			
+			right.release();
+			
+			System.out.println("Philosopher " + id + " releases right fork.");
+			
+			left.release();
+			
+			System.out.println("Philosopher " + id + " releases left fork.");
 			
 			if(nTimes > 0){
 				count++;
